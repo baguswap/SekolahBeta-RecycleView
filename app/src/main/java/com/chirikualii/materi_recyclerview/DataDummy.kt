b818b61850d79fc00b2540802a2deed3fdd3ef95
Lab-ERP-23\Lab-ERP23 <bagushot@gmail.com>
2022-11-05 17:17:21 +0700

    Recycle View

---
+++ app/src/main/java/com/chirikualii/materi_recyclerview/DataDummy.kt
@@ -0,0 +1,37 @@
+package com.chirikualii.materi_recyclerview
+
+object DataDummy  {
+
+    val listProfile = listOf<Profile>(
+
+        Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"),
+        Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        Profile(name = "Jokowi",phone = "08123345789",image = "https://asiapacificreport.nz/wp-content/uploads/2016/07/apr-jokowi-amn-inter-680wide.jpg"), Profile(name = "Mega-Chan",phone = "08123456789",image = "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjM2NDQyMTI2/megawati-sukarnoputri-39637-1-402.jpg"),
+        Profile(name = "Opung Luhut",phone = "081234567289",image = "https://static01.nyt.com/images/2015/11/12/world/12Indonesia-web/12Indonesia-web-articleLarge.jpg"),
+        )
+}
